5b5721fe7cb451a5bc332157f375286d0e18e468
henrychris <henrychris2003@gmail.com>
2021-05-06 12:20:02 +0100

    Add project files.

---
+++ DayOne/Number_Guesser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Number_Guesser
+{
+	public Number_Guesser(int min, int max)
+	{
+		Answer_Generator(min, max);
+	}
+
+	public int Answer_Generator(int min, int max)
+    {
+		Random rnd = new Random();
+		int answer = rnd.Next(min, max);
+
+		return answer;
+    }
+
+	public void 
+
+
+}
---
+++ DayOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number_Guesser ng = new Number_Guesser(2, 7);
+        }
+    }
+}
